7f2c0381967a84ce53ee6c28379ecd577ed67c85
Ted Morse <tmorse@gmail.com>
2010-01-26 14:14:04 -0800

    Start of appengine integration

--- datastore.py
+++ datastore.py
@@ -4,12 +4,16 @@ from google.appengine.ext import db
 class ImokUser(db.Model):
   account = db.UserProperty()
 
-
 class Post(db.Model):
-	user 		= db.UserProperty()
-	datetime	= db.DateTimeProperty(auto_now_add=True)
+  user 	   = db.UserProperty()
+  datetime = db.DateTimeProperty(auto_now_add=True)
 
-	lat			= db.FloatProperty()
-	lon			= db.FloatProperty()
-	message 	= db.StringProperty()
+  lat      = db.FloatProperty()
+  lon	   = db.FloatProperty()
+  message  = db.StringProperty()
 
+class SmsMessage(db.Model):
+  sha_hash     = db.StringProperty(required=True)
+  received     = db.DateTimeProperty()
+  phone_number = db.StringProperty()
+  message      = db.StringProperty()
