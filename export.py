5bddde33656e047862f86c4c48827cb3c51f40ce
trey.smith@nasa.gov <trey.smith@nasa.gov>
2010-01-30 17:28:04 -0800

    +description in kml export, cleaned up variable names

--- export.py
+++ export.py
@@ -24,12 +24,7 @@ from datastore import *
 from imokutils import *
 from imokforms import *
 
-class KmlHandler(RequestHandlerPlus):
-  def get(self):
-    kml = """
-<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://earth.google.com/kml/2.2">
-<Document>
+KML_STYLES = """
   <StyleMap id="msn_red-circle">
     <Pair>
       <key>normal</key>
@@ -68,21 +63,109 @@ class KmlHandler(RequestHandlerPlus):
       </ItemIcon>
     </ListStyle>
   </Style>
+  <Style id="sn_ylw-circle">
+    <IconStyle>
+      <scale>1.1</scale>
+      <Icon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/ylw-circle.png</href>
+      </Icon>
+      <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+    </IconStyle>
+    <ListStyle>
+      <ItemIcon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/ylw-circle-lv.png</href>
+      </ItemIcon>
+    </ListStyle>
+  </Style>
+  <Style id="sh_ylw-circle">
+    <IconStyle>
+      <scale>1.3</scale>
+      <Icon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/ylw-circle.png</href>
+      </Icon>
+      <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+    </IconStyle>
+    <ListStyle>
+      <ItemIcon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/ylw-circle-lv.png</href>
+      </ItemIcon>
+    </ListStyle>
+  </Style>
+  <StyleMap id="msn_ylw-circle">
+    <Pair>
+      <key>normal</key>
+      <styleUrl>#sn_ylw-circle</styleUrl>
+    </Pair>
+    <Pair>
+      <key>highlight</key>
+      <styleUrl>#sh_ylw-circle</styleUrl>
+    </Pair>
+  </StyleMap>
+  <Style id="sh_grn-circle">
+    <IconStyle>
+      <scale>1.3</scale>
+      <Icon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/grn-circle.png</href>
+      </Icon>
+      <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+    </IconStyle>
+    <ListStyle>
+      <ItemIcon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/grn-circle-lv.png</href>
+      </ItemIcon>
+    </ListStyle>
+  </Style>
+  <Style id="sn_grn-circle">
+    <IconStyle>
+      <scale>1.1</scale>
+      <Icon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/grn-circle.png</href>
+      </Icon>
+      <hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+    </IconStyle>
+    <ListStyle>
+      <ItemIcon>
+        <href>http://maps.google.com/mapfiles/kml/paddle/grn-circle-lv.png</href>
+      </ItemIcon>
+    </ListStyle>
+  </Style>
+  <StyleMap id="msn_grn-circle">
+    <Pair>
+      <key>normal</key>
+      <styleUrl>#sn_grn-circle</styleUrl>
+    </Pair>
+    <Pair>
+      <key>highlight</key>
+      <styleUrl>#sh_grn-circle</styleUrl>
+    </Pair>
+  </StyleMap>
 """.lstrip()
+
+class KmlHandler(RequestHandlerPlus):
+  def get(self):
+    kml = """
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.2">
+<Document>
+""".lstrip()
+    kml += KML_STYLES
     posts = Post.all().fetch(1000)
     for post in posts:
         if post.hasLocation():
+            user = post.user
             profile = ImokUser.all().filter('account =', post.user).fetch(1)[0]
+            dateTime = formatLocalFromUtc(post.datetime, profile.tz)
+            desc = template.render(settings.template_path('messageInfo.html'), self.getContext(locals()))
             kml += """
   <Placemark>
     <name>%s</name>
-    <description>fill me in</description>
+    <description><![CDATA[%s]]></description>
     <styleUrl>#msn_red-circle</styleUrl>
     <Point>
       <coordinates>%f,%f,0</coordinates>
     </Point>
   </Placemark>
-""" % (profile.getShortName(), post.lon, post.lat)
+""" % (profile.getShortName(), desc, post.lon, post.lat)
     kml += """
 </Document>
 </kml>
